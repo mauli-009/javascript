2b07979b9b5e640aff63483fcbcebee5ffebd61f
Mauli Dudhat <151643077+mauli-009@users.noreply.github.com>
2024-12-15 13:32:19 +0000

    container config done

---
+++ 01_basics/let_const_var.js
@@ -0,0 +1,18 @@
+const account_id=144553
+let accout_email="mdudhat3030@gmail.com"
+var account_pass="1234"
+account_city="bombay"
+let account_state;
+
+//account_id=2
+accout_email="bdbdb"
+account_pass="bdbso"
+account_city="pune"
+console.log(account_id);
+console.table([account_id,account_pass,accout_email,account_city,account_state])
+/*
+
+Note->prefer not to use var->becuse the issue in the block scope and functional scope
+
+
+*/
