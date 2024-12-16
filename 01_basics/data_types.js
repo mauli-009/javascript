806e0ebbcc25d6da2a7c1a4bffa80c2e2cb09adc
Mauli Dudhat <151643077+mauli-009@users.noreply.github.com>
2024-12-16 13:04:56 +0000

    container config done

---
+++ 01_basics/data_types.js
@@ -0,0 +1,32 @@
+"use strict" //traet all javascript code as newer version
+
+//alert("hello") -->we are using the node js this is not allowed
+
+
+//string datatype
+let name="hitesh"
+
+//integer
+let age=18
+//boolean
+let islog=false
+
+//number => 2 to the power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value  ->represnt of the empty value;
+//undefined=>value is not defined
+
+//symbol
+
+//object
+
+
+console.log(typeof "hitesh");
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
